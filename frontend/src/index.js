e5aca0dcdc1e21a34ea9d673ad48bae28b79c799
astralship genesis <mstefanow@gmail.com>
2018-09-23 10:33:55 +0100

    attempt

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Index from './pages/index';
+// ReactDOM.render(<Index />, document.getElementById('root'));
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux'
+
+import store, { history } from './store';
+// import { App } from 'apps/App';
+
 import Index from './pages/index';
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Index store={store}/>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
